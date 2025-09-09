81dd79aaaaad7168cf0949fb6350efe1766cc393
yao512954855 <512954855@qq.com>
2025-09-09 14:46:27 +0800

    second commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function CustomersPage() {
+    return (
+      <div className="flex min-h-screen flex-col p-6">
+        <h1 className="text-3xl font-bold">Customers</h1>
+        <p className="mt-4 text-lg text-gray-700">List of customers will be displayed here.</p>
+      </div>
+    );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function InvoicesPage() {
+  return (
+    <div className="flex min-h-screen flex-col p-6">
+      <h1 className="text-3xl font-bold">Invoice</h1>
+      <p className="mt-4 text-lg text-gray-700">Welcome to your Invoice!</p>
+    </div>
+  );
+}
